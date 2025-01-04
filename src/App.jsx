2c31ccefc117b2a7cc3d4c55ef5d3c05fe7280ea
amansingh2k21 <amansinghas1117@gmail.com>
2025-01-04 12:57:46 +0530

    added comment

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
 
   return (
     <>
+      // hiii 
       <div className='w-full h-screen' style={{ backgroundColor: Color }}>
         <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
           <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
